c3a803b915bc09bab95e32b443b6be26b35bae60
VictorTamayoBriones <66786892+VictorTamayoBriones@users.noreply.github.com>
2022-04-14 09:51:35 -0500

    main title animation

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,7 +3,16 @@ import { Hero } from "./style"
 export const Home = () =>{
     return(
         <Hero>
-            <h1>wolfcoyotl</h1>
+            <h1>w</h1>
+            <h1>o</h1>
+            <h1>l</h1>
+            <h1>f</h1>
+            <h1>c</h1>
+            <h1>o</h1>
+            <h1>y</h1>
+            <h1>o</h1>
+            <h1>t</h1>
+            <h1>l</h1>
         </Hero>
     )
 }
--- src/pages/Home/style.jsx
+++ src/pages/Home/style.jsx
@@ -13,4 +13,43 @@ export const Hero = styled.div`
     color: ${ theme.blanco };
     font-size: 70px;
     letter-spacing: 3px;
+    text-shadow: 2px 3px 1px ${ theme.negroTraslucido };
+    h1{
+        &:nth-child(odd){
+            animation: title-animation-odd 12s 7s infinite linear;
+        }
+        &:nth-child(even){
+            animation: title-animation-even 12s 7s infinite linear;
+        }
+    }
+
+    @keyframes title-animation-odd {
+        0%{
+            transform: translateY(0px);
+        }
+        25%{
+            transform: translateY(50px);
+        }
+        50%{
+            transform: translateY(-50px);
+        }
+        75%{
+            transform: translateY(0px);
+        }
+    }
+
+    @keyframes title-animation-even {
+        0%{
+            transform: translateY(0px);
+        }
+        25%{
+            transform: translateY(-50px);
+        }
+        50%{
+            transform: translateY(50px);
+        }
+        75%{
+            transform: translateY(0px);
+        }
+    }
 `;
