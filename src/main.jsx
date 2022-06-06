dbc055929c6edb5781f2b83c3226ac66c7c8cd2e
Víctor Tamayo <victortamayo509@gmail.com>
2022-06-06 13:07:22 -0500

    redirecTo link music

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { ParallaxProvider } from 'react-scroll-parallax';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ParallaxProvider>
-      <App />
-    </ParallaxProvider>
+    <BrowserRouter>
+      <ParallaxProvider>
+        <App />
+      </ParallaxProvider>
+    </BrowserRouter>
   </React.StrictMode>
 )
--- src/pages/Music/index.jsx
+++ src/pages/Music/index.jsx
@@ -1,13 +1,19 @@
 import { CardMusic, CardMusicBody, CardMusicImage } from "../../components/Card"
 import { Releases } from "./style"
 import palomazo from '../../images/palomazo.jpg';
+import { useNavigate } from 'react-router-dom';
 
 export const Music = () =>{
+
+    const navigate = useNavigate();
+
+    const redirecTo = link => window.open(link)
+
     return(
         <>
             <h3>Other releases</h3>
             <Releases>
-                <CardMusic>
+                <CardMusic onClick={ () => redirecTo('https://open.spotify.com/track/5p7FZ9fDPb4ZdSI5SD5Sdb?si=70d3dca650a14d1c') } >
                     <CardMusicImage>
                         <img src={palomazo} alt="" />
                     </CardMusicImage>
@@ -16,7 +22,8 @@ export const Music = () =>{
                         <span>Single</span>
                     </CardMusicBody>
                 </CardMusic>
-                <CardMusic>
+
+                <CardMusic onClick={ () => redirecTo('https://open.spotify.com/track/1r0qskqqzpX4mf5Hgr5xwN?si=d5023a192b014cca') } >
                     <CardMusicImage>
                         <img src={palomazo} alt="" />
                     </CardMusicImage>
@@ -25,7 +32,8 @@ export const Music = () =>{
                         <span>Single</span>
                     </CardMusicBody>
                 </CardMusic>
-                <CardMusic>
+
+                <CardMusic onClick={ ()=> redirecTo('https://open.spotify.com/track/3dP60bpBkcjSyaUJNzZMaV?si=34e48ff7c7f841d9') } >
                     <CardMusicImage>
                         <img src={palomazo} alt="" />
                     </CardMusicImage>
@@ -34,16 +42,8 @@ export const Music = () =>{
                         <span>Single</span>
                     </CardMusicBody>
                 </CardMusic>
-                <CardMusic>
-                    <CardMusicImage>
-                        <img src={palomazo} alt="" />
-                    </CardMusicImage>
-                    <CardMusicBody>
-                        <h5>Ear max</h5>
-                        <span>Single</span>
-                    </CardMusicBody>
-                </CardMusic>
-                <CardMusic>
+
+                <CardMusic onClick={ ()=> redirecTo('https://open.spotify.com/track/333F0xW4PhEySeBEBLHPzn?si=42233ced322e4054') } >
                     <CardMusicImage>
                         <img src={palomazo} alt="" />
                     </CardMusicImage>
